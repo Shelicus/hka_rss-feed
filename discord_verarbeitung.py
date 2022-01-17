2f5692575ce81a7ecffac7b96e650431f60da8e0
David <73255970+Shelicus@users.noreply.github.com>
2022-01-17 15:34:30 +0100

    update discord_verarbeitung.py
    fix

--- discord_verarbeitung.py
+++ discord_verarbeitung.py
@@ -1,25 +1,26 @@
-def messages_rss_datein(message_name):
-    list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen ]
+def messages_rss(message_name, nummer):
+    if nummer == 1:
+        list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
 
-    for x in range(len(list_message)):
+        for x in range(len(list_message)):
 
-        if message_name == "list_message_name" + f"{x}":
-            zahl = list_message[-x - 1]
-            return zahl
+            if message_name == "list_message_name" + f"{x}":
+                zahl = list_message[-x - 1]
+                return zahl
 
-def messages_rss_allgemein(message_name):
-    list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen ]
+    elif nummer == 0:
+        list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen ]
 
-    for x in range(len(list_message)):
+        for x in range(len(list_message)):
 
-        if message_name == "list_message_name" + f"{x}":
-            zahl = list_message[-x - 1]
-            return zahl
+            if message_name == "list_message_name" + f"{x}":
+                zahl = list_message[-x - 1]
+                return zahl
 
 def channel(channel_name):
-    rss_datein_id = channel-id
-    rss_allgemein_id = channel-id
-    fail_id = channel-id
+    rss_datein_id = 900101000684269589
+    rss_allgemein_id = 900101058297233438
+    fail_id = 901162532348252170
 
 
     list_channel = [rss_datein_id, rss_allgemein_id, fail_id]
