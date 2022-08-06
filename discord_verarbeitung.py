e1cc5885f149ebf688511db8c8aa41206204f879
David <73255970+Shelicus@users.noreply.github.com>
2022-08-06 23:15:20 +0200

    update discord_verarbeitung

--- discord_verarbeitung.py
+++ discord_verarbeitung.py
@@ -2,38 +2,36 @@
 # Abfrage der IDs der Nachrichten
 #--------------------------------------------------------------------------------------------------
 def messages_rss(message_name, nummer):
-    if nummer == 1:
-        list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
+    liste_message = []
 
-        for x in range(len(list_message)):
+    match nummer:
+        case 1:
+             list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
 
-            if message_name == "list_message_name" + f"{x}":
-                zahl = list_message[-x - 1]
-                return zahl
+        case 0:
+            list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
+        case 2:
+            list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
 
-    elif nummer == 0:
-        list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen ]
-
-        for x in range(len(list_message)):
-
-            if message_name == "list_message_name" + f"{x}":
-                zahl = list_message[-x - 1]
-                return zahl
+    for x in range(len(list_message)):
+        if message_name == "list_message_name" + f"{x}":
+            zahl = list_message[-x - 1]
+            return zahl
 
 #---------------------------------------------------------------------------------------------------
 # Abfrage der Channels-ID
 #--------------------------------------------------------------------------------------------------
-def channel(channel_name):
-    rss_datein_id = 900101000684269589
-    rss_allgemein_id = 900101058297233438
-    fail_id = 901162532348252170
+def channel_number(channel_name):
+    rss_datein_id = #channel_id
+    rss_allgemein_id = #channel_id
+    rss_allgemein_info_id = #channel_id
+    fail_id = #channel_id
 
+    list_channel = [rss_datein_id, rss_allgemein_id, fail_id, rss_allgemein_info_id]
 
-    list_channel = [rss_datein_id, rss_allgemein_id, fail_id]
-
-    list_channel_name = ["rss_datein_id", "rss_allgemein_id", 'fail_id']
+    list_channel_name = ["rss_datein_id", "rss_allgemein_id", "fail_id", "rss_allgemein_info_id"]
 
     for x in range(len(list_channel)):
         if channel_name == list_channel_name[x]:
-            zahl = list_channel[x]
-            return zahl
+            id = list_channel[x]
+            return id
