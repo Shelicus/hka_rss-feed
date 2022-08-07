1a041374345258d80074723ac2e124800bd69057
David <73255970+Shelicus@users.noreply.github.com>
2022-08-07 14:52:36 +0200

    Update discord_verarbeitung.py

--- discord_verarbeitung.py
+++ discord_verarbeitung.py
@@ -1,24 +1,4 @@
 #---------------------------------------------------------------------------------------------------
-# Abfrage der IDs der Nachrichten
-#--------------------------------------------------------------------------------------------------
-def messages_rss(message_name, nummer):
-    liste_message = []
-
-    match nummer:
-        case 1:
-             list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
-
-        case 0:
-            list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
-        case 2:
-            list_message = [50 Text nachrichten + 1 ZUm löschen aber nicht eintragen]
-
-    for x in range(len(list_message)):
-        if message_name == "list_message_name" + f"{x}":
-            zahl = list_message[-x - 1]
-            return zahl
-
-#---------------------------------------------------------------------------------------------------
 # Abfrage der Channels-ID
 #--------------------------------------------------------------------------------------------------
 def channel_number(channel_name):
@@ -27,11 +7,12 @@ def channel_number(channel_name):
     rss_allgemein_info_id = #channel_id
     fail_id = #channel_id
 
-    list_channel = [rss_datein_id, rss_allgemein_id, fail_id, rss_allgemein_info_id]
-
-    list_channel_name = ["rss_datein_id", "rss_allgemein_id", "fail_id", "rss_allgemein_info_id"]
-
-    for x in range(len(list_channel)):
-        if channel_name == list_channel_name[x]:
-            id = list_channel[x]
-            return id
+    match channel_name:
+        case "rss_datein_id":
+            return rss_datein_id
+        case "rss_allgemein_id":
+            return rss_allgemein_id
+        case "rss_allgemein_info_id":
+            return rss_allgemein_info_id
+        case "fail_id":
+            return fail_id
